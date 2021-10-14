73248d38a48f87c207e7b1170488df5ad1decfe5
User <stepanovdmitriy778@gmail.com>
2021-10-15 00:38:47 +0300

    last commit

--- Portfolio programmist java. Main work/TestWork123/SpringBootSocksManager/src/main/java/main/DefaultController.java
+++ Portfolio programmist java. Main work/TestWork123/SpringBootSocksManager/src/main/java/main/DefaultController.java
@@ -21,7 +21,7 @@ public class DefaultController
         if(socksQuantity >= 0) {
 
             Socks newSocks = socksRepositiry.save(socks);
-            return new ResponseEntity("Задача добавлена: id: " + newSocks.getId(), HttpStatus.OK);
+            return new ResponseEntity("Итем носки добавлен: id: " + newSocks.getId(), HttpStatus.OK);
 
         }
 
@@ -33,24 +33,26 @@ public class DefaultController
     public ResponseEntity putSocks(@RequestBody Socks socksRequest) {
         Iterable<Socks> socksIterable = socksRepositiry.findAll();
         ArrayList<Socks> socks = new ArrayList<>();
-        ArrayList currentIDs = new ArrayList();
         socksIterable.forEach(s -> socks.add(s));
         Socks newSocks = new Socks();
+        boolean isWasFirstoutcome = false;
         ResponseEntity responseEntity = new ResponseEntity("Cтандартный статус", HttpStatus.OK);
         ArrayList<String> arrayIDs = new ArrayList<>();
 
             for (Socks socksItem : socks) {
                 if ( socksRequest.getQuantity() >= 0) {
                     if (socksRequest.getColor().equals(socksItem.getColor()) && socksRequest.getCottonPart().equals(socksItem.getCottonPart())) {
-                        if(socksItem.getQuantity() - socksRequest.getQuantity() >= 0)  {
+                        if(socksItem.getQuantity() - socksRequest.getQuantity() >= 0 && isWasFirstoutcome == false)  {
                         socksItem.setQuantity(socksItem.getQuantity() - socksRequest.getQuantity());
                         newSocks = socksRepositiry.save(socksItem);
 
-                        arrayIDs.add(String.valueOf(newSocks.getId()));}
+                        arrayIDs.add(String.valueOf(newSocks.getId()));
+                        isWasFirstoutcome = true; // если убрать будет убираться во всех итемах где цвет хлопок и количество, а так в ровно количеству Quantity в запросе
+                        }
 
 
                     }
-                    responseEntity = new ResponseEntity("Итем носки обновлены: id: " + arrayIDs, HttpStatus.OK);
+                    responseEntity = new ResponseEntity("Итем носки обновлен: id: " + arrayIDs, HttpStatus.OK);
 
 
                 } else return new ResponseEntity("Сумма ухода должна быть положительной", HttpStatus.BAD_REQUEST);
